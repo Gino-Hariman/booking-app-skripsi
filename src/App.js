abf1e786f0a26aabd94887e0fdf8e809a6b8b34c
Gino Hariman <51822089+Gino-Hariman@users.noreply.github.com>
2021-12-07 16:31:51 +0700

    testing styling text input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import TextInput from "./components/TextInput";
 import StyledForm from "./styles";
 
 function App() {
   return (
     <StyledForm>
       <h1 className="font-bold font-sans text-H1">Hello</h1>
+      <TextInput />
       <form>
         <input type="text" placeholder="Full name" />
         <input type="text" placeholder="Email" />
---
+++ src/components/TextInput/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const StyledTextInput = styled.input.attrs({
+  class: "w-full border-gray-300 rounded-md py-3 px-5  max-w-md",
+})``;

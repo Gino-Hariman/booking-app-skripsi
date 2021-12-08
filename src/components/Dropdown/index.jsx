237f632ccdf06b414ff4e5ab827e6230ecd74650
Gino Hariman <51822089+Gino-Hariman@users.noreply.github.com>
2021-12-08 22:15:21 +0700

    fix icon (#6)

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -32,6 +32,21 @@ const Dropdown = ({ selected, setSelected, options }) => {
         >
           {selected ? selected : "Select Campus Location"}
           <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-black-100"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d={isActive ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+            />
+          </svg>
+
+          {/* <svg
             className="-mr-1 ml-2 h-5 w-5 fill-current text-black-100"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -43,7 +58,7 @@ const Dropdown = ({ selected, setSelected, options }) => {
               d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
               clip-rule="evenodd"
             />
-          </svg>
+          </svg> */}
         </DropdownButton>
       </div>
 

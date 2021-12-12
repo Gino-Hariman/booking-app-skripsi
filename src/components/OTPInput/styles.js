08292263be88b0686d03d9298627a5f86071aa56
Gino Hariman <ginohrmn26@gmail.com>
2021-12-12 22:22:43 +0700

    change theme

--- src/components/OTPInput/styles.js
+++ src/components/OTPInput/styles.js
@@ -2,5 +2,5 @@ import styled from "styled-components";
 
 export const StyledInput = styled.input.attrs({
   className:
-    "w-12 h-11 mr-sm py-md text-center text-black-40 border border-black-10 rounded bg-white",
+    "w-max mr-8 py-md px-md text-body text-center text-black-40 border border-black-10 rounded bg-white",
 })``;
--- src/styles/index.js
+++ src/styles/index.js
@@ -2,17 +2,17 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 const StyledForm = styled.main.attrs({
-  className: "flex flex-col h-screen justify-center items-center bg-gray-100",
+  className: "flex flex-col h-screen justify-center items-center bg-black-10",
 })`
   & {
     form {
-      ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xs`}
+      ${tw`bg-white text-center rounded py-lg px-md shadow max-w-xs`}
     }
     input {
-      ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
+      ${tw`border-black-30 mb-16 w-full border-solid border rounded py-sm px-md`}
     }
     button {
-      ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
+      ${tw`bg-success-main hover:bg-success-hover text-white font-bold py-sm px-md border border-info-pressed rounded`}
     }
   }
 `;

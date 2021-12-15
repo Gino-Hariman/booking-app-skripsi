d4ec8235d3add8d6cc234ab693d08bc503551825
Gino Hariman <51822089+Gino-Hariman@users.noreply.github.com>
2021-12-15 16:33:06 +0700

    change selected style

--- src/components/ListTime/styles.js
+++ src/components/ListTime/styles.js
@@ -17,5 +17,5 @@ export const StyledCheckBox = styled.div.attrs({
   className:
     "w-max rounded-full ml-lg mt-[16px] px-lg py-sm border text-black-40 text-Tag font-semibold border-black-10 bg-white focus:bg-primary",
 })`
-  ${({ selected }) => (selected ? tw`bg-primary` : tw`bg-danger-pressed`)}
+  ${({ selected }) => (selected ? tw`bg-primary text-white` : tw`bg-white`)}
 `;

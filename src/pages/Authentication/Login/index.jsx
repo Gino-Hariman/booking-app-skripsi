f29eb45361590cab0ec9e63218bf1d056e15dff2
Gino Hariman <51822089+Gino-Hariman@users.noreply.github.com>
2021-12-17 09:24:53 +0700

    make push notif

--- src/pages/Authentication/Login/index.jsx
+++ src/pages/Authentication/Login/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../../../components/Navbar";
-import { LeftImage, LoginContainer } from "./styles";
+import { ImageContainer, LeftImage, LoginContainer } from "./styles";
 import imgOffice from "../../../assets/image/office.jpg";
 
 const Login = () => {
   return (
     <LoginContainer>
       <Navbar />
-      <LeftImage src={imgOffice} alt="login_image" />
+      <ImageContainer>
+        <LeftImage src={imgOffice} alt="login_image" />
+      </ImageContainer>
     </LoginContainer>
   );
 };
--- src/pages/Authentication/Login/styles.js
+++ src/pages/Authentication/Login/styles.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 export const LoginContainer = styled.div.attrs({
-  className: "w-auto h-auto",
+  className: "flex flex-col flex-1 w-auto h-screen max-h-full",
+})``;
+
+export const ImageContainer = styled.div.attrs({
+  className: "flex flex-1 w-full items-center",
 })``;
 
 export const LeftImage = styled.img.attrs({
-  className: "w-2/5 mt-14 rounded-r-md ",
+  className: "w-5/12 h-5/6 rounded-r-md ",
 })``;

fb8a3a183069edd8f17948892aa38c6dd48a104b
Gino Hariman <ginohrmn26@gmail.com>
2021-12-17 23:26:29 +0700

    done make login header & small adjust

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Container, NavContainer, NavLogo } from "./styles";
 import Logo from "../../assets/image/logo_uph.png";
+import LoginButton from "./components/LoginButton";
+import LoggedButton from "./components/LoggedButton";
 
 const Navbar = () => {
   return (
     <Container>
       <NavContainer>
         <NavLogo src={Logo} alt="logo_uph" />
+        {/* <LoginButton /> */}
+        <LoggedButton />
       </NavContainer>
     </Container>
   );
--- src/pages/Authentication/Login/index.jsx
+++ src/pages/Authentication/Login/index.jsx
@@ -1,14 +1,5 @@
 import React from "react";
-import Navbar from "../../../components/Navbar";
-import {
-  Container,
-  ImageContainer,
-  InputContainer,
-  LeftImage,
-  LoginContainer,
-  RightContainer,
-} from "./styles";
-import imgOffice from "../../../assets/image/office.jpg";
+import { InputContainer } from "./styles";
 import { HeaderTitle } from "../styles";
 import DualLayout from "../../../components/DualLayout";
 import Button from "../../../components/Button";

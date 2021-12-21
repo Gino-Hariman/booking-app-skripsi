bd4ba07f493144eaa197993f57c68bcab7a8fd20
Gino Hariman <ginohrmn26@gmail.com>
2021-12-21 21:07:21 +0700

    done make book chair

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -4,12 +4,16 @@ export const H1 = styled.h1.attrs({
   className: "text-H1 font-bold text-black-100",
 })``;
 
+export const H3 = styled.h3.attrs({
+  className: "text-H3 font-medium",
+})``;
+
 export const H4 = styled.h4.attrs({
   className: "text-H4 font-medium",
 })``;
 
-export const H3 = styled.h3.attrs({
-  className: "text-H3 font-medium",
+export const H5 = styled.h5.attrs({
+  className: "text-H5 font-medium",
 })``;
 
 export const Body = styled.body.attrs({
--- src/pages/Booking/BookingAction/forms/ChairForm/ChairSection/styles.js
+++ src/pages/Booking/BookingAction/forms/ChairForm/ChairSection/styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const ChairSectionContainer = styled.div.attrs({
-  className: "p-5 text-left border-b border-black-10",
+  className: "min-w-max text-left py-5 border-y border-black-10",
 })``;
 
 export const ChairChoiceContainer = styled.div.attrs({
-  className: "flex flex-wrap mx-2",
+  className: "w-full flex flex-wrap mx-2",
 })``;
 
 export const ChairChoiceItem = styled.div.attrs({

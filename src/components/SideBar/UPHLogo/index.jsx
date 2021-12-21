c9d4d5d5420a4a7619612b416e00087705ccd448
Gino Hariman <ginohrmn26@gmail.com>
2021-12-21 22:38:26 +0700

    add logo to admin page

--- src/components/SideBar/UPHLogo/index.jsx
+++ src/components/SideBar/UPHLogo/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import logo from "../../../assets/image/logo_uph.png";
 
-const UPHLogo = () => <div style={{ height: "100px" }} />;
+const UPHLogo = () => (
+  <div
+    className="bg-no-repeat bg-center"
+    style={{ backgroundImage: `url(${logo})`, height: "100px" }}
+  />
+);
 
 export default UPHLogo;
